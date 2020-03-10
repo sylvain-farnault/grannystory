46615c0d187b31a7bc2108b8ed3bd2ff5ba7bf92
Sylvain FARNAULT <sylvain.farnault@gmail.com>
2020-03-10 11:17:05 +0100

    wip

--- app/models/granny.rb
+++ app/models/granny.rb
@@ -4,7 +4,7 @@ class Granny < ApplicationRecord
   # has_many :passsions, through: :granny_passions
   validates :name, uniqueness: true, presence: true, length: { minimum: 3 }
   validates :address, presence: true
-  validates :birth_date
+  validates :birth_date, presence: true
   validates :user, presence: true
-  validates :price, presence: true
+  validates :price, presence: true, numericality: { greater_than: 0 }
 end
--- db/migrate/20200310093318_create_grannies.rb
+++ db/migrate/20200310093318_create_grannies.rb
@@ -1,11 +1,11 @@
 class CreateGrannies < ActiveRecord::Migration[5.2]
   def change
     create_table :grannies do |t|
-      t.string :name
-      t.string :address
-      t.date :birth_date
+      t.string :name, null: false
+      t.string :address, null: false
+      t.date :birth_date, null: false
       t.references :user, foreign_key: true
-      t.float :price
+      t.float :price, null: false
 
       t.timestamps
     end
