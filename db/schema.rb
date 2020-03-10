570b6213e812706337c35fdb86f3ae8826138458
Sylvain FARNAULT <sylvain.farnault@gmail.com>
2020-03-10 17:12:25 +0100

    Add Passion model, add granny_passion model and controler

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_10_093318) do
+ActiveRecord::Schema.define(version: 2020_03_10_150525) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,21 @@ ActiveRecord::Schema.define(version: 2020_03_10_093318) do
     t.index ["user_id"], name: "index_grannies_on_user_id"
   end
 
+  create_table "granny_passions", force: :cascade do |t|
+    t.bigint "granny_id", null: false
+    t.bigint "passion_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["granny_id"], name: "index_granny_passions_on_granny_id"
+    t.index ["passion_id"], name: "index_granny_passions_on_passion_id"
+  end
+
+  create_table "passions", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -39,4 +54,6 @@ ActiveRecord::Schema.define(version: 2020_03_10_093318) do
   end
 
   add_foreign_key "grannies", "users"
+  add_foreign_key "granny_passions", "grannies"
+  add_foreign_key "granny_passions", "passions"
 end
---
+++ test/controllers/granny_passions_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class GrannyPassionsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
