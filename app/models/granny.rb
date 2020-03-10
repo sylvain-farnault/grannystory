e7d7c09cfb1cadfc139f2001368965ca7c2782f5
Sylvain FARNAULT <sylvain.farnault@gmail.com>
2020-03-10 11:02:35 +0100

    wip

--- app/models/granny.rb
+++ app/models/granny.rb
@@ -1,3 +1,10 @@
 class Granny < ApplicationRecord
   belongs_to :user
+  # has_many :granny_passions, dependent: :destroy
+  # has_many :passsions, through: :granny_passions
+  validates :name, uniqueness: true, presence: true, length: { minimum: 3 }
+  validates :address, presence: true
+  validates :birth_date
+  validates :user, presence: true
+  validates :price, presence: true
 end
