0c3a07e4647150b9fdcd43b38f4f29c9966b1d78
Laurent Fourquet <fourquet@live.fr>
2020-03-10 14:23:23 +0100

    grannycreatenew

--- app/controllers/grannies_controller.rb
+++ app/controllers/grannies_controller.rb
@@ -1,6 +1,36 @@
 class GranniesController < ApplicationController
+before_action :set_granny, only: [:show, :edit, :update, :destroy]
+skip_before_action :authenticate_user!, only: [:show, :index]
 
   def index
     @grannies = Granny.all
   end
+
+  def create
+    @granny = Granny.new(granny_params)
+    @granny.user = current_user
+    if @granny.save
+      redirect_to grannies_path
+    else
+      render :new
+    end
+  end
+
+  def new
+    @granny = Granny.new
+  end
+
+
+private
+
+def set_granny
+    @granny = Granny.find(params[:id])
+  end
+
+
+def granny_params
+    params.require(:granny).permit(:name, :address, :birth_date, :price)
+  end
+
+
 end
