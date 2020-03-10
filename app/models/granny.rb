a712a22af4dc6df58110ebf5ee0ee668ef0b46cc
Sylvain FARNAULT <sylvain.farnault@gmail.com>
2020-03-10 12:24:34 +0100

    add model with validations and migration for granny. Add has_many relation in user model

--- app/models/granny.rb
+++ app/models/granny.rb
@@ -2,7 +2,7 @@ class Granny < ApplicationRecord
   belongs_to :user
   # has_many :granny_passions, dependent: :destroy
   # has_many :passsions, through: :granny_passions
-  validates :name, uniqueness: true, presence: true, length: { minimum: 3 }
+  validates :name, uniqueness: true, presence: true, length: { minimum: 2 }
   validates :address, presence: true
   validates :birth_date, presence: true
   validates :user, presence: true
