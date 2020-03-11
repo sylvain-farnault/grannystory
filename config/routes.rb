02e8b1979e06ece381748205f7713123377ef91a
Laurent Fourquet <fourquet@live.fr>
2020-03-11 17:23:18 +0100

    Wip

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
 
-  resources :grannies, only:[:index, :create, :new, :show, :destroy, :edit, :update]
+  resources :grannies, only:[:index, :create, :new, :show, :destroy, :edit, :update] do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :bookings, only: [:create]
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_10_150525) do
+ActiveRecord::Schema.define(version: 2020_03_11_141741) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "granny_id", null: false
+    t.date "start_date", null: false
+    t.date "end_date", null: false
+    t.float "price", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["granny_id"], name: "index_bookings_on_granny_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
 
   create_table "grannies", force: :cascade do |t|
     t.string "name", null: false
@@ -53,6 +65,8 @@ ActiveRecord::Schema.define(version: 2020_03_10_150525) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bookings", "grannies"
+  add_foreign_key "bookings", "users"
   add_foreign_key "grannies", "users"
   add_foreign_key "granny_passions", "grannies"
   add_foreign_key "granny_passions", "passions"
