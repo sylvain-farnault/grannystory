543785600d1d4cb91878f1290f51e505a90a2d00
Laurent Fourquet <fourquet@live.fr>
2020-03-12 18:32:09 +0100

    enelevelePGsearch

--- app/models/granny.rb
+++ app/models/granny.rb
@@ -13,11 +13,4 @@ class Granny < ApplicationRecord
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
 
-include PgSearch::Model
-  pg_search_scope :search_by_name_and_passions,
-    against: [ :name, :passions ],
-    using: {
-      tsearch: { prefix: true } # <-- now `superman batm` will return something!
-    }
-
 end
