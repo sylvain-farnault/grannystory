2df5c8f89e54b4f45f7d011230047fb869b47c5c
Sylvain FARNAULT <sylvain.farnault@gmail.com>
2020-03-13 15:57:43 +0100

    booking quite ok

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -6,6 +6,6 @@ class Booking < ApplicationRecord
 validates :user, presence: true
 validates :granny, presence: true
 validates :start_date, presence: true
-validates :end_date, presence: true
+validates :duration, presence: true
 validates :price, presence: true
 end
